662ad1bc09ac82c9bdf40a5a99e82ef7ef1a15bd
Shinpei <react-native@kakao.com>
2020-04-22 00:03:56 +0900

    Simplify

--- src/hooks/useMediaQuery/index.ts
+++ src/hooks/useMediaQuery/index.ts
@@ -1,7 +1,10 @@
 import React, { useEffect, useMemo, useState, useCallback, useRef } from "react";
+
 interface IResponsibleHookConfig {
     [type: string]: string;
 }
+
+type Order = "ASC" | "DESC";
 
 function checkBrowser() {
     if (typeof window !== undefined) {
@@ -19,38 +22,35 @@ function checkPC() {
     }
 }
 
-function checkConfig(config: any) {
+function checkConfig(config: IResponsibleHookConfig) {
     if (!config) {
         throw new Error("Need to config");
     }
 }
 
-function sortConfigWithSize(config: IResponsibleHookConfig, order: "ASC" | "DESC" = "ASC") {
-    const sortedConfig = {};
+function sortConfigWithSize(config: IResponsibleHookConfig, order: Order = "ASC") {
     const configEntries = Object.entries(config);
-    const sortedConfigEntries = configEntries.sort((before, after) => {
+    configEntries.sort((before, after) => {
         const beforeSize = Number(before[1].replace("px", ""));
         const afterSize = Number(after[1].replace("px", ""));
-        if (!before || !afterSize) {
+        if (!beforeSize || !afterSize || Number.isNaN(beforeSize) || Number.isNaN(afterSize)) {
             throw new Error("You need to use px in size");
         }
-        return order === "ASC" ? beforeSize - afterSize : afterSize - beforeSize;
+        if (order === "ASC")
+            return beforeSize - afterSize;
+        return afterSize - beforeSize;
     });
-    sortedConfigEntries.forEach(item => {
-        Object.assign(sortedConfig, { [item[0]]: item[1] });
-    });
-    return sortedConfig;
+
+    return Object.fromEntries(sortConfig);
 }
 
-export const useMediaQuery = (config: IResponsibleHookConfig, initial: string): [string, any?] => {
+export const useMediaQuery = (config: IResponsibleHookConfig = {}, initial: string): [string, any?] => {
     try {
         if (!checkBrowser()) {
             throw new Error("It is not broswer environment, you need to use this in browser environment");
         }
         const [mediaType, setMediaType] = useState<string>("");
         const latestType = useRef<string>("");
-
-        checkConfig(config);
 
         const sortedConfig = useMemo(() => sortConfigWithSize(config, "DESC"), [config]);
         const configKeys = useMemo(() => Object.keys(sortedConfig), [sortedConfig]);
@@ -63,9 +63,8 @@ export const useMediaQuery = (config: IResponsibleHookConfig, initial: string): 
                         return type;
                     });
                 } else {
-                    const index = configKeys.findIndex(value => value === type);
-                    if (index && index - 1 >= 0) {
-                        setMediaType(configKeys[index - 1]);
+                    if (configKeys.includes(type)) {
+                        setMediaType(type);
                     } else {
                         setMediaType(initial);
                     }
@@ -113,6 +112,11 @@ export const useMediaQuery = (config: IResponsibleHookConfig, initial: string): 
                 };
             }
         }, [mediaQuerys]);
+
+        useEffect(() => {
+            checkConfig(config);
+        }, [config]);
+
         return [mediaType];
     } catch (error) {
         return ["Error", error];
