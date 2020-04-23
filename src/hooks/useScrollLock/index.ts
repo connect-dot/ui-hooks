45848bfae9b2ad7ab8c70a0a47d7891293e7a159
geonwoo.jeong <doscmDev@gmail.com>
2020-04-23 15:21:03 +0900

    refactor: fix problems that can occur when modal is opened

--- src/hooks/useScrollLock/index.ts
+++ src/hooks/useScrollLock/index.ts
@@ -1,15 +1,39 @@
 import { useEffect, useState } from "react";
 
+type TScrollOptions = {
+    overflowX: string;
+    overflowY: string;
+};
+
 export const useScrollLock = (on: boolean) => {
     useEffect(() => {
-        const body = window.document.getElementsByTagName("body")[0];
+        const body = document.body;
+
+        const [overflowYOfBody, setOverflowYOfBody] = useState<string | undefined>(body?.style.overflowY);
+
+        const [overflowXOfBody, setOverflowXOfBody] = useState<string | undefined>(body?.style.overflowX);
+
+        const currentScrollOptions = {
+            overflowX: overflowXOfBody,
+            overflowY: overflowYOfBody,
+        };
+
+        const scrollLockOptions = {
+            overflowX: "hidden",
+            overflowY: "hidden",
+        };
+
+        const handleBodyScroll = (scrollOptions: TScrollOptions) => {
+            body.style.overflowX = scrollOptions.overflowX;
+            body.style.overflowY = scrollOptions.overflowY;
+        };
         if (on) {
-            body && (body.style.overflow = "hidden");
+            overflowYOfBody && handleBodyScroll(currentScrollOptions);
         } else {
-            body && (body.style.overflow = "scroll");
+            overflowYOfBody && handleBodyScroll(scrollLockOptions);
         }
         return () => {
-            body && (body.style.overflow = "scroll");
+            overflowYOfBody && handleBodyScroll(scrollLockOptions);
         };
     }, [on]);
 };
