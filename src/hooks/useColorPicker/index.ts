3201286e2c35c02fd28e3c78296a52ba89ae4e7c
Jeontaeyun <jow1813@naver.com>
2020-07-15 23:05:51 +0900

    Feat : implement useColorPick detail logic

--- src/hooks/useColorPicker/index.ts
+++ src/hooks/useColorPicker/index.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { rgbToHex } from "../../shared/utils";
+import { rgbToHex, checkCanvas } from "../../shared/utils";
 
 type ImageType = File | string;
 type ColorPosition = { x: number; y: number };
@@ -9,17 +9,19 @@ const INIT_COLOR = "#ffffff";
 
 const useColorPicker = (img: ImageType, position: ColorPosition = { x: 0, y: 0 }, initColor = INIT_COLOR) => {
     const [color, setColor] = useState(initColor);
-    const [loading, setLoading] = useState();
+    const [loading, setLoading] = useState(true);
     const extractColorFromImage = useCallback(async () => {
         try {
+            setLoading(true);
             await mountCanvas();
             const url = await convertImageToString(img);
-            await draw(url, position);
-            const color = await extractColor();
+            const color = await extractColor(url, position, initColor);
             color && setColor(color);
             await unMountCanvas();
+            setLoading(false);
         } catch (error) {
             console.error(error);
+            setLoading(false);
             throw error;
         }
     }, [img]);
@@ -43,6 +45,8 @@ async function convertImageToString(imgURL: ImageType) {
 async function mountCanvas() {
     try {
         const canvas = document.createElement("canvas");
+        if (!checkCanvas(canvas)) throw new Error("This browser doesn't support canvas apis");
+
         canvas.id = CANVAS_ID;
         document.body.appendChild(canvas);
 
@@ -68,27 +72,22 @@ async function unMountCanvas() {
     }
 }
 
-async function extractColor() {
-    try {
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
-
-async function draw(url: string, position: ColorPosition) {
-    const { x, y } = position;
-    const canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement;
-    const context = canvas.getContext("2d");
-    const img = new Image();
-    img.src = url;
-    context &&
-        (img.onload = function () {
-            context.drawImage(img, 0, 0);
-            const pixel = context.getImageData(x, y, 1, 1).data;
-            const isTransparent = validateTransparent(pixel);
-            const hexCode = "#" + ("000000" + rgbToHex(pixel[0], pixel[1], pixel[2])).slice(-6);
-        });
+async function extractColor(url: string, position: ColorPosition, initColor: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const { x, y } = position;
+        const canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement;
+        const context = canvas.getContext("2d");
+        const img = new Image();
+        img.src = url;
+        context &&
+            (img.onload = function () {
+                context.drawImage(img, 0, 0);
+                const pixel = context.getImageData(x, y, 1, 1).data;
+                const isTransparent = validateTransparent(pixel);
+                const hexCode = "#" + ("000000" + rgbToHex(pixel[0], pixel[1], pixel[2])).slice(-6);
+                isTransparent ? Promise.resolve(hexCode) : Promise.resolve(initColor);
+            });
+    });
 }
 
 function validateTransparent(pixel: Uint8ClampedArray) {
