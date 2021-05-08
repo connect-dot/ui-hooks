5544fc8cab74389b760805af53d27c4e955e793d
Jeontaeyun <jow1813@naver.com>
2021-05-08 21:06:51 +0900

    [FIX] Fix some of useMediaQuery

--- src/hooks/useOuterClickNotification/index.ts
+++ src/hooks/useOuterClickNotification/index.ts
@@ -1,20 +1,17 @@
 import { useCallback, useEffect } from "react";
 
 const useOuterClickNotification = (onOuterClick: Function, innerRef: React.RefObject<HTMLElement>) => {
-    const handleClick = useCallback((event: globalThis.MouseEvent) => {
+    const handleOuterClick = useCallback((event: globalThis.MouseEvent) => {
         const shouldNotifyOuterClick =
             innerRef.current && event.target && !innerRef.current.contains(event.target as Node);
 
         if (shouldNotifyOuterClick) onOuterClick();
-
     }, []);
 
     useEffect(() => {
-        if (innerRef.current) {
-            document.addEventListener("click", handleClick, true);
-            return () => document.removeEventListener("click", handleClick, true);
-        }
-    }, [onOuterClick, innerRef, handleClick]);
+        document.addEventListener("click", handleOuterClick, true);
+        return () => document.removeEventListener("click", handleOuterClick, true);
+    }, [onOuterClick, innerRef, handleOuterClick]);
 };
 
 export default useOuterClickNotification;
